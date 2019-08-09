b044644dbedbe863d89312da7f1b314a25fa9fd9
Atenea Espuna <42168237+AteneaEspuna@users.noreply.github.com>
2019-08-08 23:56:20 -0500

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,41 @@
+//Atenea Leon Espuna Programación en Ingeniería Tarea 3
+//MULTIPLICATION TABLES
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int number;
+    number = 5;
+    printf("Multiplication table of %d\n\n\n", number);
+    printf("%d x 1 = %d\n", number, 0 * numer);
+    printf("%d x 1 = %d\n", number, 1 * number);
+    printf("%d x 2 = %d\n", number, 2 * number);
+    printf("%d x 3 = %d\n", number, 3 * number);
+    printf("%d x 4 = %d\n", number, 4 * number);
+    printf("%d x 5 = %d\n", number, 5 * number);
+    printf("%d x 6 = %d\n", number, 6 * number);
+    printf("%d x 7 = %d\n", number, 7 * number);
+    printf("%d x 8 = %d\n", number, 8 * number);
+    printf("%d x 1 = %d\n", number, 9 * number);
+    printf("%d x 9 = %d\n\n\n\n\n", number, 10 * number);
+
+    number = 9;
+    printf("Tabla del %d\n\n\n", number);
+    printf("%d x 1 = %d\n", number, 0 * numer);
+    printf("%d x 1 = %d\n", number, 1 * numer);
+    printf("%d x 2 = %d\n", number, 2 * number);
+    printf("%d x 3 = %d\n", number, 3 * number);
+    printf("%d x 4 = %d\n", number, 4 * number);
+    printf("%d x 5 = %d\n", number, 5 * number);
+    printf("%d x 6 = %d\n", number, 6 * number);
+    printf("%d x 7 = %d\n", number, 7 * number);
+    printf("%d x 8 = %d\n", number, 8 * number);
+    printf("%d x 9 = %d\n", number, 9 * number);
+    printf("%d x 1 = %d\n", number, 10 * number);
+
+
+    return 0;
+}
